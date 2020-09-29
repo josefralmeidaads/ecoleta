9a2eb375d0a5ee3a97112b090b8caca44c443667
Jose Francisco de Almeida Filho <69639482+josefralmeidaads@users.noreply.github.com>
2020-09-29 09:49:03 -0300

    ecoleta

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,8 +2,10 @@ import express from 'express';
 
 const routes = express.Router();
 
-routes.get('/users', (request, response) => {
+
+routes.get('/', (request, response) => {
     return response.json({ message: 'Joia' });
 })
+
 
 export default routes;
